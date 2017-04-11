f0ed417974df69839902b84e714a76ea4a6d6d68
Skdi <jddhghf@gmail.com>
2017-04-11 11:14:51 -0500

    Update roll.cpp

--- roll.cpp
+++ roll.cpp
@@ -1,59 +1,44 @@
-#include "StdAfx.h"
-#include <iostream>
-#include <iomanip>
+#include "stack.h"
 
-#include "Stack.h"
-
-using namespace std;
-
-
-        Stack::Stack( void ){
-			  InitializeStack();
-        }
-
-
-    Stack::~Stack(){}
-        
-        
-        void Stack::InitializeStack( ){
-            top = 0;
+    Stack::Stack( ){
+              top=0;
         }
 
 
         void Stack::Pop( int& poppedValue )
         {
-			if (!StackEmpty()){
-	        top--;
+            if (!StackEmpty()){
+            top--;
             poppedValue = elements[top];
-			PrintStack();
-		}
-			else
-			PrintStack();
-        }     
-	    void Stack::PrintStack( )
+            PrintStack();
+        }
+            else
+            PrintStack();
+        }
+        void Stack::PrintStack( )
         {
-			cout << endl << endl;
-            cout << "+ Top of Stack  +" << endl;
-			cout << "=+=+=+=+=+=+=+=+=" << endl;
-			cout << endl;
+            cout << endl << endl;
+
+            cout << "=+=+=+=+=+=+=+=+=" << endl;
+            cout << endl;
             if ( StackEmpty() )
-                cout << "The stack is empty." << endl;
+                cout << "stack vacio." << endl;
             else{
                 for ( int ii = top - 1; ii >= 0; ii-- ){
-                    cout << "At index[" << ii << "] = "
+                    cout << "posicion[" << ii << "] = "
                          << elements[ii] << endl;
                 } // for
             } // else
-			cout << endl;
-			cout << "=+=+=+=+=+=+=+=+=" << endl;
-            cout << "+Bottom of Stack+" << endl << endl;
-        }//End Function PrintStack
+            cout << endl;
+            cout << "=+=+=+=+=+=+=+=+=" << endl;
 
-        void Stack::Push( int pushedValue ) 
+        }
+
+        void Stack::Push( int pushedValue )
         {
             elements[top] = pushedValue;
             top++;
-			PrintStack();
+            PrintStack();
         }
         bool Stack::StackEmpty( )
         {
@@ -61,25 +46,23 @@ using namespace std;
         }
         bool Stack::StackFull( )
         {
-            return (top == MAX_STACK_SIZE);
+            return (top == 10);
         }
-		
-	//funcion del ejercicio ROLL
-		void Stack::Roll(int i)
-		{
-			if (top == 0)
-				cout << "pila vacia" << endl << endl;
-			else if (top == 1)
-				cout << "solo hay un elemento" << endl << endl;
-			else
-			{
-			int temp;
-			//switch entre los elementos	
-			temp = elements[top-i];
-			elements[top-i] = elements[top-i-1];
-			elements[top-i-1] = temp;
-			PrintStack( );
-			}
-		}
-		
-		}
+
+    //funcion del ejercicio ROLL
+        void Stack::Roll(int i)
+        {
+            if (top == 0)
+                cout << "pila vacia" << endl << endl;
+            else if (top == 1)
+                cout << "solo hay un elemento" << endl << endl;
+            else
+            {
+            int temp;
+            //switch entre los elementos
+            temp = elements[top-i];
+            elements[top-i] = elements[top-i-1];
+            elements[top-i-1] = temp;
+            PrintStack( );
+            }
+        }
