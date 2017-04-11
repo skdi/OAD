021c53cb6725fdab54e59246d620e76aa2051192
Skdi <jddhghf@gmail.com>
2017-04-10 19:20:28 -0500

    Update addcomas.cpp

--- addcomas.cpp
+++ addcomas.cpp
@@ -1,47 +1,43 @@
-
 #include <iostream>
 #include <string>
-#include <cctype>
+#include <cctype>//header para funciones de clasificacion
 using namespace std;
 
-/*Function prototypes*/
+
 string addCommas(string digits);
 
-/*Main program*/
+
 int main(){
+    
     while(true){
         string digits;
-        cout << "Enter a number: ";
+        cout << "ingrese el numero: ";
         getline(cin, digits);
-        if (digits == "") break;
-        cout << addCommas(digits) << endl;
+        if (digits == "") break;//si no ingresaste un numero ,sale
+        cout << addCommas(digits) << endl;//llamada a la funcion
     }
     return 0;
 }
 
-/*Function: addCommas
- *Usage: string addCommas(string digits)
- *---------------------------------------
- *Adds commas to long digits
- */
+
 string addCommas(string digits){
-    string result;
-    if (digits.length() > 3){
+    string result;//string copia
+    if (digits.length() > 3){//si tiene mas de 3 digitos
         int t = 0;
 
-        for(int i = digits.length() - 1; i >= 0; i --){
-            result = digits[i] + result;
-            t++;
-            if(t%3 == 0){
+        for(int i = digits.length() - 1; i >= 0; i --){//recorrido del string
+            result = digits[i] + result;//copio todos los numeros
+            t++;//actualizo posicion
+            if(t%3 == 0){//cada 3 numeros agrego una coma
                 result = ',' + result;
 
             }
         }
-        if(result[0] == ','){
+        if(result[0] == ','){//si ingresaste una coma al inicio la borrara
             result.erase(0, 1);
         }
     } else {
-        result = digits;
+        result = digits;//si es menor de 3 digitos devuelve el mismo valor
     }
-    return result;
+    return result;//retorno el string copia con las comas
 }
