e4ddad2da984eb1f87ed8c467438086a2c9ac583
Skdi <jddhghf@gmail.com>
2017-04-20 20:26:16 -0500

    Update filitas.cpp

--- filitas.cpp
+++ filitas.cpp
@@ -2,11 +2,16 @@
 /////////////////////////////////////////////////////////////////////////////////////////
 //Header
 /////////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+using namespace std;
 
 class cripto{
   private:
-    string clave="";
+    int clave="";
   public:
-    cripto(string mensaje,string clave);
-    cripto(string mensaje,string clave);
+    cripto(string mensaje,int clave);//genera clave
+    cripto(string mensaje,int clave);//recibe clave
+    string alfabeto="abcdefghijklmnopqrstuvwxyz ";
 }
