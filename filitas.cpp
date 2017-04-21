ffc143b4c376571dd4764b6afa6d68c6ec656ec7
Skdi <jddhghf@gmail.com>
2017-04-20 20:46:57 -0500

    Update filitas.cpp

--- filitas.cpp
+++ filitas.cpp
@@ -1,6 +1,6 @@
 //empezemos :V
 /////////////////////////////////////////////////////////////////////////////////////////
-//Header
+//Header cripto.h
 /////////////////////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <math.h>
@@ -13,10 +13,30 @@ class cripto{
     string mensaje="";
   public:
     string alfabeto="abcdefghijklmnopqrstuvwxyz ";
-    cripto(int clave);//genera clave
+    cripto(int clave);
+    bool validarclave(int clave);
+    bool validarmensaje(string mensaje);
     string codifica(string mensaje);
     string decodifica(string mensaje);
     void setclave();
     int getclave();
     
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//cpp cripto.cpp
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#include "cripto.h"
+
+bool cripto::validarclave(int clave){
+  if(clave>=1)
+    return true;
+  else return false;
 }
+bool cripto::validarmensaje(string mensaje){
+  if(!mensaje)
+    return false;
+  else return true;
+}
+
