fb08f230561b6a16068c11556d4a9a3a789fa76e
Skdi <jddhghf@gmail.com>
2017-04-24 18:51:00 -0500

    Update histograma.cpp

--- histograma.cpp
+++ histograma.cpp
@@ -4,31 +4,26 @@
 
 using namespace std;
 
-class  compare {
-public:
-   bool operator() (int a, int b) {
-      return a / 10 < b / 10;
-   }
-};
-
 int main() {
-   map<int, double, compare> histogram;
-   ifstream f("archivo.txt");
-   int count;
-   while (f) {
-      int val;
-      f >> val;
-      if (!f)
+   map<int, int> histogram;//creo una variable de tipo map , donde recibe el primer elemento para llegar al segundo y los compara
+   ifstream f("datos.txt");//abro el archivo de datos
+   int count;//inicializo un contador
+   while (f) {//mientras el archivo este abierto
+      int val;//creo una variable auxiliar
+      f >> val;//le paso los valores del archivo a mi variable aux
+      if (!f)//si mi archivo ya no esta abierto ,termino el ciclo
           break;
-      histogram[val]++;
-      count++;
+      histogram[val]++;//voy sumando el valor del archivo a la variable tipo map y actualizo su posicion
+      count++;//aumento el valor de mi contador
    }
-   for (int i = 0; i <= 100; i+=10) {
-      cout << i << "s:";
-      for (int j = 0; j < histogram[i]; j++) {
-         cout << "*" ;
+   for (int i = 0; i <= 100; i+=10) {//genero un for enntre el tamaño del problema
+      cout << i << "s:";//imprimo los primeros datos(aunentandolos de 10 en 10)
+      for (int j = 0; j < histogram[i]; j++) {//creo un segundo bloque que recorra el histograma
+         cout << "*" ;//e imprima un asterisco por cada vez que se encuentre con ese valor
       }
       cout << endl;
    }
    return 0;
 }
+
+//3er ejercicio ./a.out <draw.in
