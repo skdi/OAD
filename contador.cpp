2a196f0327cd58ffe813387bb778d0e5ca0471f7
Skdi <jddhghf@gmail.com>
2017-04-24 20:24:03 -0500

    Update contador.cpp

--- contador.cpp
+++ contador.cpp
@@ -4,25 +4,23 @@
 #include <string>
 using namespace std;
 
-int contarWords(string);
-
 int main()
 {
+    map<int,int> m;
     string s;
-    map<int, int> m;
-    ifstream archivo("english_words.txt");
-    if(!archivo.is_open())
-    {
-        cout<<"Imposible abrir el archivo de texto text.txt\n";
-        return 0;
+    string narchivo="english_words.txt";
+    ifstream archivo;
+    archivo.open(narchivo.c_str());
+    if(!archivo.fail()){
+        while(archivo>>s){ // Mientras se lean líneas,
+            m[s.size()]++;
+        }
+        archivo.close();
     }
-    while(archivo>>s){ // Mientras se lean líneas,
-        m[s.size()]++;
-    while(archivo.is_open()){
-        cout<<m[i];
-        i++;}
-    return 0;
+
 }
+
+
 //https://drive.google.com/drive/folders/0B3PCb9zwHKQkRXFZUWFydFlQeEk
 
 
