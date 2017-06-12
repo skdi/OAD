d29aa1a1dd33df2240be1523421388ac14ff1ef6
Skdi <jddhghf@gmail.com>
2017-06-12 18:46:14 -0500

    Create hash.cpp

---
+++ hash.cpp
@@ -0,0 +1,174 @@
+#ifndef HASH_H
+#define HASH_H
+#include <iostream>
+using namespace std;
+
+const int TABLE_SIZE=128;
+class hash{
+private:
+    int key;
+    int value;
+    hash *next;
+public:
+    hash(int key,int value);
+    int getkey(){return key;}
+    int get_value(){return value;}
+    void set_value(int value){this->value=value;}
+    hash *getnext(){return next;}
+    void set_next(hash* next){this->next=next;}
+};
+
+class hashmap{
+private:
+    hash **table;
+public:
+    hashmap(){
+        table=new hash*[TABLE_SIZE];
+        for(int i=0;i<TABLE_SIZE;i++)
+            table[i]=NULL;
+    }
+    int get(int key){
+        int hash1=(key%TABLE_SIZE);
+        if(table[hash1]==NULL){
+            return -1;
+        }
+        else{
+            hash *entry=table[hash1];
+            while(entry!=NULL && entry->getkey()!=key)
+                entry=entry->getnext();
+            if(entry==NULL)
+                return -1;
+            else return entry->get_value();
+        }
+    }
+    void put(int key,int value);
+    void remove(int key);
+    ~hashmap(){
+        for(int i=0;i<TABLE_SIZE;i++)
+            if(table[i]!=NULL){
+                hash *prev=NULL;
+                hash *entry=table[i];
+                while(entry!=NULL){
+                    prev=entry;
+                    entry=entry->getnext();
+                    delete prev;
+                }
+            }
+        delete[] table;
+    }
+
+};
+
+
+
+
+
+
+
+
+#endif // HASH_H
+
+
+#include "hash.h"
+
+hash::hash(int key, int value){
+    this->key=key;
+    this->value=value;
+    this->next=NULL;
+
+}
+
+void hashmap::put(int key, int value){
+    int hash1=key%TABLE_SIZE;
+    int cont=0;
+    if(table[hash1]==NULL){
+        table[hash1]=new hash(key,value);
+        return;
+    }else{
+        while(cont<=hash1){//recorrido hasta la posicion del hash
+            table[hash1]=table[hash1]->getnext();
+            cont++;
+        }
+
+        hash *nuevo=new hash(key,value);
+        table[hash1]->set_next(nuevo);
+    }
+
+}
+
+void hashmap::remove(int key){
+    int hash1=key%TABLE_SIZE;
+    hash *mihash=table[hash1];
+    if(mihash==NULL){
+        cout<<"vacio"<<endl;
+        return;
+    }else{
+        while(mihash->getnext()!=NULL){
+            if(mihash->getkey()==key){
+                hash *temp=mihash;//guardo la posicion anterior
+                mihash=mihash->getnext();//poisicion actual
+                hash *temp2=mihash->getnext();//posicion sig
+                temp->set_next(temp2);
+                delete mihash;
+            }
+            mihash=mihash->getnext();
+        }
+    }
+
+
+}
+
+
+
+
+#include <iostream>
+#include "hash.h"
+
+using namespace std;
+
+int main()
+{
+    hashmap* mimap=new hashmap();
+    mimap->put(3,2);
+    mimap->put(3,5);
+    mimap->put(141,6);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
