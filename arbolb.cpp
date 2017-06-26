3dc04d405a1fb82a86d6fdc2bb49c123e6686798
Skdi <jddhghf@gmail.com>
2017-06-26 17:05:21 -0500

    Update arbolb.cpp

--- arbolb.cpp
+++ arbolb.cpp
@@ -1,88 +1,4 @@
 /*tarea hacerlo con templates y ponerle destructor >:v */
-
-
-#include "arbolb.h"
-
-
-
-bool arbolb::insert(int valor){
-    node **n;
-    if(find(n,valor))
-        return false;
-    *n=new node(valor);
-    return true;
-}
-
-bool arbolb::find(node **& n,int value){
-    n=&root;
-    while((*n)){
-        if((*n)->value==value){
-            return true;
-        }
-        n=&(*n)->child[(*n)->value<value];
-        return false;
-    }
-
-}
-
-void arbolb::imprimir(node *root){
-    if(root){
-        cout<<root->value<<endl;
-        if(root->child[0]){
-            imprimir(root->child[0]);
-
-        }
-        if(root->child[1]){
-            imprimir(root->child[1]);
-
-        }
-}
-    else return;
-}
-
-
-
-bool arbolb::find(int value){
-   node **n;
-   return find(n,value);
-
-}
-
-
-
-#ifndef ARBOLB_H
-#define ARBOLB_H
-#include <iostream>
-
-using namespace std;
-
-struct node{
-    int value;
-    node *child[2];
-    node(){
-        value=0;
-        child[0]=child[1]=NULL;
-    }
-    node(int valor){
-        value=valor;
-        child[0]=child[1]=NULL;
-    }
-};
-
-class arbolb{
-private:
-    node *root;
-public:
-    arbolb(){root=NULL;}
-    bool insert(int valor);
-    void eliminar();
-    void imprimir(node *root);
-    node *getroot(){return root;}
-    bool find(int value);
-    bool find(node **&n, int value);
-};
-
-#endif // ARBOLB_H
 #include <iostream>
 #include "arbolb.h"
 
@@ -94,9 +10,134 @@ int main()
     tree->insert(1);
     tree->insert(5);
     tree->insert(3);
+    tree->insert(3);
+    tree->insert(3);
+    tree->insert(3);
+    tree->insert(3);
+    tree->insert(3);
     tree->insert(7);
-    tree->imprimir(tree->getroot());
+    cout<<"altura prro: " <<tree->altura()<<endl;
+    tree->indorder(tree->getroot());
+   // tree->imprimir();
     cout << "Hello World!" << endl;
     return 0;
 }
+#include "arbolb.h"
+
+
+template <typename T>
+bool arbolb::insert(T valor){
+    node **n;
+    if(find(n,valor))
+        return false;
+    *n=new node(valor);
+    return true;
+}
+template <typename T>
+bool arbolb::find(node<T> **&n, T value){
+    n=&root;
+    while((*n)){
+        if((*n)->value==value){
+            return true;
+        }
+        n=&(*n)->child[(*n)->value<value];
+        return false;
+    }
+
+}
+template <typename T>
+int arbolb::altura(){
+    node<T> *temp=root->child[0];
+    node<T> *temp2=root->child[1];
+    int contador=0;
+    int contador2=0;
+    while(temp->child[0]!=NULL || temp2->child[1]!=NULL){
+        //recorrido hacia la izquierda
+        if(temp->child[0]!=NULL){
+            contador++;
+            temp=temp->child[0];
+        }
+        //recorrido hacia la derecha
+        else if(temp2->child[1]!=NULL){
+            contador2++;
+            temp2=temp2->child[1];
+        }
+    }
+    if(contador>contador2)
+        return contador;
+    else return contador2;
+}
+template <typename T>
+void arbolb::imprimir(){
+    queue<node *> q;
+    q.push(root);
+    while(!q.empty()){
+        node *n=q.front();
+        q.pop();
+        cout<<n->value<<endl;
+        if(n->child[0])
+            q.push(n->child[0]);
+        if(n->child[1])
+            q.push(n->child[1]);
+    }
+
+}
+template <typename T>
+void arbolb::indorder(node<T> *root){
+    if(root==NULL)
+        return;
+    else{
+        cout<<root->value<<endl;
+        if(root->child[0])
+            indorder(root->child[0]);
+        if(root->child[1])
+            indorder(root->child[1]);
+}
+
+}
+
+
+template <typename T>
+bool arbolb::find(T value){
+   node **n;
+   return find(n,value);
+
+}
+#ifndef ARBOLB_H
+#define ARBOLB_H
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+template <typename T>
+struct node{
+    T value;
+    node *child[2];
+    node(){
+        value=0;
+        child[0]=child[1]=NULL;
+    }
+    node(int valor){
+        value=valor;
+        child[0]=child[1]=NULL;
+    }
+};
+template <typename T>
+class arbolb{
+private:
+    node<T> *root;
+public:
+    arbolb(){root=NULL;}
+    bool insert(T valor);
+    void eliminar();
+    int altura();
+    void imprimir();
+    void indorder(node<T> *root);
+    node<T> *getroot(){return root;}
+    bool find(T value);
+    bool find(node<T> **&n, T value);
+};
+
+#endif // ARBOLB_H
 
